4d56a7f162f6d35c114af064231cfa7cd86aa9b7
ilshat16 <illliat@mail.ru>
2023-11-01 12:16:29 +0300

    Use JdbcTemplate

--- src/main/java/ru/ilshat/springcrud/config/SpringConfig.java
+++ src/main/java/ru/ilshat/springcrud/config/SpringConfig.java
@@ -1,5 +1,9 @@
 package ru.ilshat.springcrud.config;
 
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -46,5 +50,22 @@ public class SpringConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    	
+    	dataSource.setDriverClassName("org.postgresql.Driver");
+    	dataSource.setUrl("jdbc:postgresql://localhost:5432/spring_mvc");
+    	dataSource.setUsername("postgres");
+    	dataSource.setPassword("password");
+    	
+    	return dataSource;
+    }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+    	return new JdbcTemplate(dataSource());
     }
 }
